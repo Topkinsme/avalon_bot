86631dbf5c49f2cc7c59bb705fd5b38e15a2cd89
Topkinsme <46650272+Topkinsme@users.noreply.github.com>
2023-12-22 17:36:55 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,104 @@
+#bot
+token="<Your Token>"
+
+#server
+server_id=<Your Server ID>
+
+#people
+owner_id=<Your user id>
+self_id=<Your bot user id>
+
+#channels
+lobby_id= <Lobby channel id>
+peochannel_id = <Person announcement channel id>
+annchannel_id = <Announcements channel id>
+
+#roles
+signed_up_id= <Signedup player role id>
+players_id= <Players role id>
+
+#rounds
+rounds=[
+    [2,3,2,3,3],
+    [2,3,4,3,4],
+    [2,3,3,4,4],
+    [3,4,4,5,5],
+    [3,4,4,5,5]
+]
+#rolelists
+rolelists=[
+    #Mode 0 = Classic
+    ["Loyal Servant of Arthur",
+    "Loyal Servant of Arthur",
+    "Assassin",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred",
+    "Loyal Servant of Arthur",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred"
+    ],
+    #Mode 1 = Merlin only
+    ["Merlin",
+    "Loyal Servant of Arthur",
+    "Assassin",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred",
+    "Loyal Servant of Arthur",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred"
+    ],
+    #Mode 2= Merlin, Percival and Morgana
+    ["Merlin",
+    "Percival",
+    "Morgana",
+    "Loyal Servant of Arthur",
+    "Assassin",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred",
+    "Loyal Servant of Arthur",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred",
+    ],
+    #Mode 3= Oberon only (Min 7)
+    ["Loyal Servant of Arthur",
+    "Loyal Servant of Arthur",
+    "Assassin",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred",
+    "Loyal Servant of Arthur",
+    "Oberon",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred",
+    "Loyal Servant of Arthur",
+    ],
+    #Mode 4= Merlin, Percival, Morgana and Mordred (Min 7)
+    ["Merlin",
+    "Loyal Servant of Arthur",
+    "Morgana",
+    "Percival",
+    "Assassin",
+    "Loyal Servant of Arthur",
+    "Mordred",
+    "Loyal Servant of Arthur",
+    "Loyal Servant of Arthur",
+    "Minion of Mordred",
+    ],
+    #Mode 5= Merlin, Percival, Morgana, Mordred and Oberon (Min 9)
+    ["Merlin",
+    "Loyal Servant of Arthur",
+    "Morgana",
+    "Percival",
+    "Assassin",
+    "Loyal Servant of Arthur",
+    "Mordred",
+    "Loyal Servant of Arthur",
+    "Oberon",
+    "Loyal Servant of Arthur",
+    ]
+]
+
+emotes=["-",":one:",":two:",":three:",":four:",":five:",":six:",":seven:",":eight:",":nine:",":keycap_ten:"]
